97499b8ed0b60a5f9cc79c068a11ea3613fea8b5
domagoo <michaelaw7195@gmail.com>
2023-01-02 02:51:16 -0500

    Search-Box Functional Component

--- monster-rolodex/src/components/search-box/search-box.component.jsx
+++ monster-rolodex/src/components/search-box/search-box.component.jsx
@@ -1,21 +1,13 @@
-import { Component } from "react";
-
 import './search-box.styles.css';
 
-class SearchBox extends Component {
-
-    render(){
-
+const SearchBox =({className, placeholder, onChangeHandler})=>{
     return (
       <input 
-    //   className='search-box'
-      class= {`search-box ${this.props.className}`}
+      class= {`search-box ${className}`}
       type = 'search'
-      placeholder= {this.props.placeholder}
-      onChange={this.props.onChangeHandler}
+      placeholder= {placeholder}
+      onChange={onChangeHandler}
       />
     )
 }
-}
-
 export default SearchBox;
