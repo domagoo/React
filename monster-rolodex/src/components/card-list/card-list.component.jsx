44c073e2e6f952a9e4cc0f7bf65be286a93356e7
domagoo <michaelaw7195@gmail.com>
2023-01-02 02:50:58 -0500

    Card-List Functional Component

--- monster-rolodex/src/components/card-list/card-list.component.jsx
+++ monster-rolodex/src/components/card-list/card-list.component.jsx
@@ -1,30 +1,15 @@
-import { Component } from "react";
-
-
 import Card from '../card/card.component';
 import './card-list.styles.css';
 
 
 
-class CardList extends Component{
+const CardList =({monsters})=> (
     
-    render(){
-        const { monsters} = this.props;
+    <div className="card-list"> 
+    {monsters.map((monster) => {
+    return  <Card monster = {monster} />;
+    })}
+    </div>
+    );
 
-        return( 
-        <div className="card-list"> 
-        {monsters.map((monster) => {
-    
-
-        return (
-            <Card monster = {monster} />
-        );
-           
-        })}
-        </div>
-
-        );
-    }
-
-}
 export default CardList;
